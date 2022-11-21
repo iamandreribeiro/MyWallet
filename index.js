fb5f4d3a9c4b6145817f01d8b35902afcc1bf971
iamandreribeiro <andreribeiroorafael@gmail.com>
2022-11-21 15:10:26 -0300

    refactor: adding controllers

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { MongoClient } from 'mongodb';
-import Joi from 'joi';
 import cors from 'cors';
-import bcrypt from 'bcrypt';
-import dayjs from 'dayjs';
+import { signUp, signIn } from './controllers/users.controller.js';
+import { newRecord, showRecords } from './controllers/records.controller.js';
 
 const app = express();
 
@@ -22,98 +21,15 @@ try {
 }
 
 const db = client.db("mywallet");
+export const usersCollection = db.collection("users");
+export const recordsCollection = db.collection("records");
 
-app.post("/sign-up", (req, res) => {
-    const {name, email, password, passwordConfirmation} = req.body;
+app.post("/sign-up", signUp);
 
-    const validation = Joi.object({
-        name: Joi.string().required().min(3),
-        email: Joi.string().email().required(),
-        password: Joi.string().required(),
-        passwordConfirmation: Joi.string().required().valid(password)
-    });
+app.post("/sign-in", signIn);
 
-    const validateSignUp = validation.validate({name, email, password, passwordConfirmation}, {abortEarly: false});
+app.post("/new-record", newRecord);
 
-    if(validateSignUp.error) {
-        return res.status(422).send(validateSignUp.error.details.map(value => value.message));
-    } else {
-        db.collection("users").find({email}).toArray().then((user) => {
-            if(user.length > 0) {
-                return res.sendStatus(409);
-            } else {
-                const encrypt = bcrypt.hashSync(password, 10);
-                db.collection("users").insertOne({
-                    name: name,
-                    email: email,
-                    password: encrypt
-                });
-
-                return res.sendStatus(201);
-            }
-        });
-    }    
-});
-
-app.post("/sign-in", async (req, res) => {
-    const {email, password} = req.body;
-
-    const user = await db.collection("users").findOne({email});
-
-    if(user && bcrypt.compareSync(password, user.password)) {        
-        return res.status(200).send(user);
-    } else {
-        return res.sendStatus(404);
-    }
-});
-
-app.post("/new-record", (req, res) => {
-    const {value, description, email, type} = req.body;
-
-    const validation = Joi.object({
-        value: Joi.number().required(),
-        description: Joi.string().required(),
-        email: Joi.string().email().required(),
-        type: Joi.string().valid("input", "output")
-    });
-
-    const validateRecord = validation.validate(req.body, {abortEarly: false});
-
-    if(validateRecord.error) {
-        return res.status(422).send(validateRecord.error.details.map(value => value.message));
-    } else {
-        db.collection("users").findOne({email}).then((user) => {
-            if(user) {
-                db.collection("records").insertOne({
-                    date: dayjs().format('DD/MM'),
-                    value: value,
-                    description: description,
-                    email: email,
-                    type: type
-                });
-
-                return res.sendStatus(201);
-            } else {
-                return res.sendStatus(404);
-            }
-        });        
-    }
-});
-
-app.get("/show-records", async (req, res) => {
-    const {email} = req.headers;
-    const filteredRecords = [];
-    const user = await db.collection("users").findOne({email});
-
-    await db.collection("records").find({}).toArray().then((records) => {
-        records.forEach((record) => {
-            if(record.email === email) {
-                filteredRecords.push(record);
-            }
-        });
-    });
-
-    return res.status(201).send({filteredRecords, user});
-});
+app.get("/show-records", showRecords);
 
 app.listen(process.env.PORT, () => console.log(`Server running in port ${process.env.PORT}`));
