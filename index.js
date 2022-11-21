2877b52f301061dadd414e088054eef53fc997f6
iamandreribeiro <andreribeiroorafael@gmail.com>
2022-11-21 00:36:41 -0300

    refactor: changing var name

--- index.js
+++ index.js
@@ -24,17 +24,17 @@ try {
 const db = client.db("mywallet");
 
 app.post("/sign-up", (req, res) => {
-    const {name, email, password, confirmPassword} = req.body;
-    console.log(name, email, password, confirmPassword);
+    const {name, email, password, passwordConfirmation} = req.body;
+    console.log(name, email, password, passwordConfirmation);
 
     const validation = Joi.object({
         name: Joi.string().required().min(3),
         email: Joi.string().email().required(),
         password: Joi.string().required(),
-        confirmPassword: Joi.string().required().valid(password)
+        passwordConfirmation: Joi.string().required().valid(password)
     });
 
-    const validateSignUp = validation.validate({name, email, password, confirmPassword}, {abortEarly: false});
+    const validateSignUp = validation.validate({name, email, password, passwordConfirmation}, {abortEarly: false});
 
     if(validateSignUp.error) {
         return res.status(422).send(validateSignUp.error.details.map(value => value.message));
